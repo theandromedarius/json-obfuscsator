dc23b7388359cc193335d41964289498f3357fe7
bagusrahmadani <baraelektrozone@gmail.com>
2023-08-28 13:43:30 +0700

    fix karakter pemformatan text

--- scripts/button.js
+++ scripts/button.js
@@ -1,12 +1,10 @@
-const copyButton = q('#copyButton'), pasteButton = q('#pasteButton'), clearButton = q('#clearButton'), submitButton = q('#submitButton'), fileInput = q('#fileInput'), exportButton = q('#exportButton');
+const copyButton = q('#copyButton'), pasteButton = q('#pasteButton'), submitButton = q('#submitButton'), fileInput = q('#fileInput'), exportButton = q('#exportButton');
 
 submitButton.addEventListener('click', () => { copyButton.innerHTML = 'Copy'; copyButton.disabled = false; outputText.textContent = filterText(textInput.value); exportButton.style.marginRight = '';});
 
 copyButton.addEventListener('click', () => { outputText.select(); document.execCommand('copy'); copyButton.innerHTML = '&#10003; Copied'; copyButton.disabled = true; exportButton.style.marginRight = "25px"; });
 
 pasteButton.addEventListener('click', async () => { textInput.value = await navigator.clipboard.readText(); });
-
-clearButton.addEventListener('click', resetPage);
 
 fileInput.addEventListener('change', handleFileSelect);
 
